3d135bcc2dd7daf82878ae7865d099cdfb4362ce
Dave <imdaveead@gmail.com>
2019-01-12 23:18:40 -0500

    move everything around

--- generate.js
+++ generate.js
@@ -1,11 +1,21 @@
+// generates auto adding links
+
 function genLink(type, url) {
     return "https://elemental.davecode.me/#addpack=" + btoa(type + ";" + url);
 }
-/*
 Array.from(document.querySelectorAll(".soundpack-link")).forEach(x => {
     x.href = genLink("soundpack", x.href);
+    x.onclick = (ev) => {
+        ev.preventDefault();
+        // lets the game use window.close()
+        window.open(x.href);
+    };
 });
 Array.from(document.querySelectorAll(".theme-link")).forEach(x => {
     x.href = genLink("theme", x.href);
+    x.onclick = (ev) => {
+        ev.preventDefault();
+        // lets the game use window.close()
+        window.open(x.href);
+    };
 });
-*/
